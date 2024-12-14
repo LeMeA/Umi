9df3499367bfd529f1641c1e20df9493fed3c99c
LeMeA <exploit.vag@gmail.com>
2024-12-15 02:25:42 +0300

    Some cleanup

--- launcher.py
+++ launcher.py
@@ -1,11 +1,13 @@
 from bot.bot import Bot
-import asyncio
 
+import os
+from dotenv import load_dotenv
 
-def main() -> None:
+def main():
     bot = Bot()
-    bot.run()
+    bot.run(token=os.getenv('BOT_TOKEN'))
 
 
 if __name__ == "__main__":
+    load_dotenv()
     main()
